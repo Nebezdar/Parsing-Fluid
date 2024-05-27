6bb4bcbe39bde06fad55fde47927f0f4f29431b8
Aziz Akhmedov <aziz.akh@yandex.ru>
2024-05-27 16:04:18 +0300

    mod for big data

--- app/Http/Controllers/ParsingController.php
+++ app/Http/Controllers/ParsingController.php
@@ -10,18 +10,28 @@ class ParsingController extends Controller
     public function parsingExcel($file)
 
     {
+
         $reader = IOFactory::createReader('Xlsx');
         $spreadsheet = $reader->load($file);
 
         $sheet = $spreadsheet->getActiveSheet();
 
         $highestRow = $sheet->getHighestDataRow();
+        $urls = [];
+        set_time_limit(0);
+        for ($row = 1; $row <= $highestRow; $row++) {
+            $urls[] = [
+                'row' => $row,
+                'url' => "https://" . mb_strtolower($sheet->getCell('B' . $row)->getValue())
+            ];
+        }
 
-        for ($row = 2; $row <= $highestRow; $row++) {
-            $url = $sheet->getCell('B' . $row)->getValue();
-
+        $results = array_map(function ($data) {
+            $url = $data['url'];
 
             $ch = curl_init();
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 400);
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_HEADER, true);
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
@@ -31,10 +41,17 @@ class ParsingController extends Controller
             $finalStatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
             curl_close($ch);
 
-            $sheet->setCellValue('I' . $row, $finalUrl);
-            $sheet->setCellValue('J' . $row, $finalStatus);
+            return [
+                'row' => $data['row'],
+                'finalUrl' => $finalUrl,
+                'finalStatus' => $finalStatus
+            ];
+        }, $urls);
 
-        }
+        array_map(function ($result) use ($sheet) {
+            $sheet->setCellValue('I' . $result['row'], $result['finalUrl']);
+            $sheet->setCellValue('J' . $result['row'], $result['finalStatus']);
+        }, $results);
 
         $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
         $pathName = storage_path() . "/parsing.xlsx";
